edf8b8c336e21d79b029e980c857da9279a7aac3
alexs <pinealex1000@gmail.com>
2022-04-28 15:51:54 -0500

    ciclos.js

---
+++ JavaScript/ciclos/assets/js/main.js
@@ -0,0 +1,28 @@
+/* for
+for ...in
+.forEach
+*/
+
+//Imprimir la frase ("hola mundo" 10 veces)
+//(iniciamos variable; condición; modificacion del contador)
+/* for(let i=1; i<=10; i++){
+    console.log("Hola mundo", i); */
+    //debugger;
+//}
+
+//iteracion: una vuelta de cada ciclo
+
+//While
+/* let control = 1;
+while (control <= 5) {
+    console.log('Hola desde un ciclo while', control);
+
+    control++;//modificador de la variable de control
+} */
+//do while
+let i = 1;
+do {
+    console.log("hola desde un do while",i)
+    i++
+    
+} while (i<=5);
